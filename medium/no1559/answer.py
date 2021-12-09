f0928188e1ee3faa29f6599e800f00dc249fba65
gteu <goto.mithom@gmail.com>
2021-12-10 01:14:39 +0900

    add no1559

---
+++ medium/no1559/answer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                stack = []
+                stack.append((i, j, None, None))
+                while(stack):
+                    x, y, p_x, p_y = stack.pop()
+                    if visited[x][y]:
+                        if p_x is not None and p_y is not None:
+                            return True
+                    else:
+                        visited[x][y] = True
+                        for (n_x, n_y) in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]:
+                            if 0 <= n_x < m and 0 <= n_y < n and grid[n_x][n_y] == grid[x][y]:
+                                if not visited[n_x][n_y]:
+                                    stack.append((n_x, n_y, x, y))
+                                elif (p_x, p_y) != (n_x, n_y):
+                                    stack.append((n_x, n_y, x, y))
+        return False
