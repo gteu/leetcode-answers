f75a41858f48fbc628ecf9c4386088c823447ef0
gteu <goto.mithom@gmail.com>
2021-12-10 10:35:46 +0900

    add no1684

---
+++ easy/no1684/answer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        count = 0
+        for word in words:
+            for c in word:
+                if c not in allowed:
+                    count += 1
+                    break
+        return len(words) - count
