36c0bbf432ba3a73c9e7466469598e74058d1eaf
gteu <goto.mithom@gmail.com>
2021-12-12 12:03:45 +0900

    add no0658

---
+++ medium/no0658/answer.py
@@ -0,0 +1,5 @@
+# One-liner solution
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        return sorted(sorted(arr, key=lambda y: abs(x - y))[:k])
